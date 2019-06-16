fb0fe8b2bdc5cd8444e719a874b8cab6c8a44042
James Fielder <james@fielder.dev>
2019-06-16 22:53:38 +0100

    Simplify builder

--- builder.sh
+++ builder.sh
@@ -1,12 +1,3 @@
 #!/usr/bin/env bash
 
-for f in *.coco
-do
-    coconut $f
-done
-
-watchmedo shell-command \
-    --patterns="*.coco" \
-    --recursive \
-    --command='coconut "${watch_src_path}"' \
-    .
+coconut -w -l "$(dirname "$0")"
